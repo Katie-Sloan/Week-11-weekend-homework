f93b1409b123f1230a17f592b464a26e54b777c5
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-09 17:26:31 +0000

    Can get flight number

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -3,12 +3,28 @@ import java.util.ArrayList;
 public class Flight {
 
     private ArrayList<Passenger> passengers;
+    private String flightNumber;
+//    private Plane plane;
 
-    public Flight() {
+    public Flight(String flightNumber) {
         this.passengers = new ArrayList<Passenger>();
+        this.flightNumber = flightNumber;
+//        this.plane = new Plane(null);
     }
 
     public int passengerCount() {
         return this.passengers.size();
     }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
+
+//    public Plane getPlane() {
+//        return this.plane;
+//    }
+
+
+
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -6,15 +6,27 @@ import static org.junit.Assert.assertEquals;
 public class FlightTest {
 
     Flight flight;
+//    Plane plane;
 
     @Before
     public void setUp() {
-        flight = new Flight();
+        flight = new Flight("FR756");
+//        plane = new Plane(PlaneType.BOEING747);
     }
 
     @Test
     public void passengerCountStartsAt0() {
         assertEquals(0, flight.passengerCount());
+    }
+
+//    @Test
+//    public void hasPlane() {
+//        assertEquals(plane, flight.getPlane());
+//    }
+
+    @Test
+    public void canGetFlightNumber() {
+        assertEquals("FR756", flight.getFlightNumber());
     }
 
 }
