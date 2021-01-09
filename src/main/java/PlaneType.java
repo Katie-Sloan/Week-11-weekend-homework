526fa9313c52e63a71ee0a8cb865b08f20884dbf
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-09 16:30:20 +0000

    can get plane type

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,21 @@
+public enum PlaneType {
+
+    BOEING747(416, 100000);
+
+    private final int passengerCapacity;
+    private final int totalWeight;
+
+    PlaneType(int passengerCapacity, int totalWeight) {
+        this.passengerCapacity = passengerCapacity;
+        this.totalWeight = totalWeight;
+    }
+
+//    public int getPassengerCapacity(){
+//        return this.passengerCapacity;
+//    }
+//
+//    public int getTotalWeight(){
+//        return this.totalWeight;
+//    }
+
+}
---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaneTest {
+
+    Plane plane;
+
+    @Before
+    public void setUp(){
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void canGetType(){
+        assertEquals(PlaneType.BOEING747, plane.getType());
+    }
+
+}
